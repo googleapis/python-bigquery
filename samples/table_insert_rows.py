060419c27ea7474a05fe35131e7223724b255e95
jmgreger <43762185+jmgreger@users.noreply.github.com>
2020-08-03 12:34:38 -0400

    Update insert_rows.py for more efficient streaming
    Added in the use of selected_fields=schema to explicitly define the schema. This prevents excessive calling of table metadata when streaming large volumes of data.
    
    There may be a better way to showcase this in the code, but I wanted to put this out there for review.

--- samples/table_insert_rows.py
+++ samples/table_insert_rows.py
@@ -12,23 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START bigquery_table_insert_rows]
 
-def table_insert_rows(table_id):
+from google.cloud import bigquery
 
-    # [START bigquery_table_insert_rows]
+# Construct a BigQuery client object.
+client = bigquery.Client()
 
-    from google.cloud import bigquery
+# TODO(developer): Set table_id to the ID of the model to fetch.
+table_id = "your-project.your_dataset.your_table"
+#Explicitly define the schema. (Optional)
+schema = [
+    bigquery.SchemaField("col_1", "STRING"),
+    bigquery.SchemaField("col_2", "INTEGER"),
+    ]
+rows_to_insert = [(u"Phred Phlyntstone", 32), (u"Wylma Phlyntstone", 29)] #populate data for entry
 
-    # Construct a BigQuery client object.
-    client = bigquery.Client()
-
-    # TODO(developer): Set table_id to the ID of the model to fetch.
-    # table_id = "your-project.your_dataset.your_table"
-
-    table = client.get_table(table_id)  # Make an API request.
-    rows_to_insert = [(u"Phred Phlyntstone", 32), (u"Wylma Phlyntstone", 29)]
-
-    errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    # [END bigquery_table_insert_rows]
+try:
+    if 'schema' not in globals():
+        schema = client.get_table(table_id).schema  # Get table schema if none provided
+    client.insert_rows(table_id, selected_fields=schema, rows_to_insert)  # Stream data to BQ
+    print("New rows have been added.")
+except ValueError:
+    print("Table’s schema is not set or rows is not a Sequence.")
+        
+# [END bigquery_table_insert_rows]

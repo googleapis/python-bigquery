045596e503d168692236f49bf82f63899b75f57b
Peter Lamut <inbox@peterlamut.com>
2020-09-30 17:59:08 +0200

    Undelete failing test, but mark as skipped

--- tests/unit/enums/test_standard_sql_data_types.py
+++ tests/unit/enums/test_standard_sql_data_types.py
@@ -59,3 +59,16 @@ def test_standard_sql_types_enum_members(enum_under_test, gapic_enum):
     for name in ("STRUCT", "ARRAY"):
         assert name in gapic_enum.__members__
         assert name not in enum_under_test.__members__
+
+
+@pytest.mark.skip(reason="Code generator issue, the docstring is not generated.")
+def test_standard_sql_types_enum_docstring(enum_under_test, gapic_enum):
+    assert "STRUCT (int):" not in enum_under_test.__doc__
+    assert "BOOL (int):" in enum_under_test.__doc__
+    assert "TIME (int):" in enum_under_test.__doc__
+
+    # All lines in the docstring should actually come from the original docstring,
+    # except for the header.
+    assert "An Enum of scalar SQL types." in enum_under_test.__doc__
+    doc_lines = enum_under_test.__doc__.splitlines()
+    assert set(doc_lines[1:]) <= set(gapic_enum.__doc__.splitlines())

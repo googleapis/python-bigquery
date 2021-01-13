10d6bb6ae39f8f5a92d020673d033f0572becc03
Tim Swast <swast@google.com>
2021-01-13 09:28:59 -0600

    fix system test that sets wrong property

--- google/cloud/bigquery/job/base.py
+++ google/cloud/bigquery/job/base.py
@@ -662,7 +662,9 @@ class _JobConfig(object):
     def __setattr__(self, name, value):
         """Override to be able to raise error if an unknown property is being set"""
         if not name.startswith("_") and not hasattr(type(self), name):
-            raise AttributeError("Property {} is unknown for {}.".format(name, type(self)))
+            raise AttributeError(
+                "Property {} is unknown for {}.".format(name, type(self))
+            )
         super(_JobConfig, self).__setattr__(name, value)
 
     @property

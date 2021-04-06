017ce93530671c0d0b48157b8fcf2010d73a74b5
Tim Swast <swast@google.com>
2021-04-06 16:45:44 -0500

    fix import order

--- samples/snippets/test_update_with_dml.py
+++ samples/snippets/test_update_with_dml.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 from google.cloud import bigquery
+import pytest
 
 from conftest import resource_prefix
 import update_with_dml

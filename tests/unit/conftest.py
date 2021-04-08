440ac82d87f9b6c4adc9addf8867623e22de6778
Jim Fulton <jim@jimfulton.info>
2021-04-08 14:14:47 -0600

    We don't actually need to avoid opentelemetry
    And a 3.6 test dependened on it.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,3 @@
-import contextlib
-import google.cloud.bigquery.opentelemetry_tracing
-import mock
 import pytest
 
 from .helpers import make_client
@@ -24,18 +21,3 @@ def DS_ID():
 @pytest.fixture
 def LOCATION():
     yield "us-central"
-
-
-@pytest.fixture(autouse=True, scope="session")
-def bypass_opentelemetry_tracing():
-    @contextlib.contextmanager
-    def create_span(name, attributes=None, client=None, job_ref=None):
-        # Make existing test code that checks _get_final_span_attributes work:
-        google.cloud.bigquery.opentelemetry_tracing._get_final_span_attributes(
-            attributes, client, job_ref
-        )
-        yield
-
-    # Note that we have to mock in client, because it uses a from import. :/
-    with mock.patch("google.cloud.bigquery.client.create_span", create_span):
-        yield

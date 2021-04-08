1349d4b1419717364484e68cf98227b0dd14bd86
Jim Fulton <jim@jimfulton.info>
2021-04-08 09:43:39 -0600

    Bypass opentelemetry tracing in unit tests.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,3 +1,6 @@
+import contextlib
+import google.cloud.bigquery.opentelemetry_tracing
+import mock
 import pytest
 
 from .helpers import make_client
@@ -16,3 +19,18 @@ def PROJECT():
 @pytest.fixture
 def DS_ID():
     yield "DATASET_ID"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def bypass_opentelemetry_tracing():
+    @contextlib.contextmanager
+    def create_span(name, attributes=None, client=None, job_ref=None):
+        # Make existing test code that checks _get_final_span_attributes work:
+        google.cloud.bigquery.opentelemetry_tracing._get_final_span_attributes(
+            attributes, client, job_ref
+        )
+        yield
+
+    # Note that we have to mock in client, because it uses a from import. :/
+    with mock.patch("google.cloud.bigquery.client.create_span", create_span):
+        yield

f3707148ec8ab23b1c5e60c716e85fd99a20715b
Peter Lamut <inbox@peterlamut.com>
2021-05-03 17:46:05 +0200

    Expand *all* pyarrow pins.

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ extras = {
         "grpcio >= 1.32.0, < 2.0dev",
         "pyarrow >= 1.0.0, < 5.0dev",
     ],
-    "pandas": ["pandas>=0.23.0", "pyarrow >= 1.0.0, < 4.0dev"],
-    "bignumeric_type": ["pyarrow >= 3.0.0, < 4.0dev"],
+    "pandas": ["pandas>=0.23.0", "pyarrow >= 1.0.0, < 5.0dev"],
+    "bignumeric_type": ["pyarrow >= 3.0.0, < 5.0dev"],
     "tqdm": ["tqdm >= 4.7.4, <5.0.0dev"],
     "opentelemetry": [
         "opentelemetry-api >= 0.11b0",

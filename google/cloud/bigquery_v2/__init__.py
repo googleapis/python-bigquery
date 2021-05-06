904c39a95819ab53f501332b0125a87cd672b20f
yoshi-automation <yoshi-automation@google.com>
2021-05-06 09:43:00 +0000

    fix: add async client to %name_%version/init.py chore: add autogenerated snippets chore: remove auth, policy, and options from the reserved names list feat: support self-signed JWT flow for service accounts chore: enable GAPIC metadata generation chore: sort subpackages in %namespace/%name/init.py
    PiperOrigin-RevId: 372197450
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed May 5 13:39:02 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 83a7e1c8c2f7421ded45ed323eb1fda99ef5ea46
    Source-Link: https://github.com/googleapis/googleapis/commit/83a7e1c8c2f7421ded45ed323eb1fda99ef5ea46

--- google/cloud/bigquery_v2/__init__.py
+++ google/cloud/bigquery_v2/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +15,8 @@
 #
 
 
+from .services.model_service import ModelServiceAsyncClient
+
 from .types.encryption_config import EncryptionConfiguration
 from .types.model import DeleteModelRequest
 from .types.model import GetModelRequest
@@ -29,19 +30,18 @@ from .types.standard_sql import StandardSqlField
 from .types.standard_sql import StandardSqlStructType
 from .types.table_reference import TableReference
 
-
 __all__ = (
-    'DeleteModelRequest',
-    'EncryptionConfiguration',
-    'GetModelRequest',
-    'ListModelsRequest',
-    'ListModelsResponse',
-    'Model',
-    'ModelReference',
-    'PatchModelRequest',
-    'StandardSqlDataType',
-    'StandardSqlField',
-    'StandardSqlStructType',
-    'TableReference',
+'DeleteModelRequest',
+'EncryptionConfiguration',
+'GetModelRequest',
+'ListModelsRequest',
+'ListModelsResponse',
+'Model',
+'ModelReference',
 
+'PatchModelRequest',
+'StandardSqlDataType',
+'StandardSqlField',
+'StandardSqlStructType',
+'TableReference',
 )
--- google/cloud/bigquery_v2/types/model_reference.py
+++ google/cloud/bigquery_v2/types/model_reference.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import proto  # type: ignore
 
 
@@ -28,7 +26,6 @@ __protobuf__ = proto.module(
 
 class ModelReference(proto.Message):
     r"""Id path of a model.
-
     Attributes:
         project_id (str):
             Required. The ID of the project containing
@@ -42,11 +39,18 @@ class ModelReference(proto.Message):
             maximum length is 1,024 characters.
     """
 
-    project_id = proto.Field(proto.STRING, number=1)
-
-    dataset_id = proto.Field(proto.STRING, number=2)
-
-    model_id = proto.Field(proto.STRING, number=3)
+    project_id = proto.Field(
+        proto.STRING,
+        number=1,
+    )
+    dataset_id = proto.Field(
+        proto.STRING,
+        number=2,
+    )
+    model_id = proto.Field(
+        proto.STRING,
+        number=3,
+    )
 
 
 __all__ = tuple(sorted(__protobuf__.manifest))

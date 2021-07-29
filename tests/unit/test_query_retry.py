0598197864b0602f8743c2adeb66ef0286a9568e
Jim Fulton <jim@jimfulton.info>
2021-07-29 11:13:27 -0400

    Added test (and to existing test) to make sure we can call result multiple times

--- tests/unit/test_query_retry.py
+++ tests/unit/test_query_retry.py
@@ -1,4 +1,9 @@
+import datetime
+
 import mock
+import pytest
+
+import google.api_core.exceptions
 
 from .helpers import make_connection
 
@@ -24,13 +29,13 @@ def test_retry_failed_jobs(sleep, client):
         else:
             response['jobReference'] = dict(jobId=path.split('/')[-1],
                                             projectId='PROJECT')
-        print(response)
         return response
 
     conn = client._connection = make_connection()
     conn.api_request.side_effect = api_request
 
     job = client.query("select 1")
+
     orig_job_id = job.job_id
     result = job.result()
     assert result.total_rows == 1
@@ -39,10 +44,6 @@ def test_retry_failed_jobs(sleep, client):
     # The job adjusts it's job id based on the id of the last attempt.
     assert job.job_id != orig_job_id
     assert job.job_id == conn.mock_calls[3][2]['data']['jobReference']['jobId']
-
-    # each of the first four calls was for a different job.
-    assert len(set(call[2]['data']['jobReference']['jobId']
-                   for call in conn.mock_calls[:4])) == 4
 
     # We had to sleep three times
     assert len(sleep.mock_calls) == 3
@@ -53,3 +54,80 @@ def test_retry_failed_jobs(sleep, client):
     # They're at most 2 * (multiplier**(number of sleeps - 1)) * initial
     # The default multiplier is 2
     assert max(c[1][0] for c in sleep.mock_calls) <= 8
+
+    # We can ask for the result again:
+    responses = [
+        dict(rows=[{'f': [{'v': '1'}]}], totalRows='1'),
+        ]
+    orig_job_id = job.job_id
+    result = job.result()
+    assert result.total_rows == 1
+    assert not responses  # We made all the calls we expected to.
+
+    # We wouldn't (and didn't) fail, because we're dealing with a successful job.
+    # So the job id hasn't changed.
+    assert job.job_id == orig_job_id
+
+
+@mock.patch('google.api_core.retry.datetime_helpers')
+@mock.patch('time.sleep')
+def test_retry_failed_jobs_after_retry_failed(sleep, datetime_helpers, client):
+    """
+    If at first you don't succeed, maybe you will later. :)
+    """
+    conn = client._connection = make_connection()
+
+    datetime_helpers.utcnow.return_value = datetime.datetime(2021, 7, 29, 10, 43, 2)
+
+    err = dict(reason='rateLimitExceeded')
+    def api_request(method, path, query_params=None, data=None, **kw):
+        calls = sleep.mock_calls
+        if calls:
+            datetime_helpers.utcnow.return_value += datetime.timedelta(
+                seconds=calls[-1][1][0]
+            )
+        response = dict(status=dict(state='DONE', errors=[err], errorResult=err))
+        response['jobReference'] = data['jobReference']
+        return response
+
+    conn.api_request.side_effect = api_request
+
+    job = client.query("select 1")
+    orig_job_id = job.job_id
+
+    with pytest.raises(google.api_core.exceptions.RetryError):
+        job.result()
+
+    # We never fot a successful job, so the job id never changed:
+    assert job.job_id == orig_job_id
+
+    # We failed because we couldn't succeed after 120 seconds.
+    # But we can try again:
+    responses = [
+        dict(status=dict(state='DONE', errors=[err], errorResult=err)),
+        dict(status=dict(state='DONE', errors=[err], errorResult=err)),
+        dict(status=dict(state='DONE', errors=[err], errorResult=err)),
+        dict(status=dict(state='DONE')),
+        dict(rows=[{'f': [{'v': '1'}]}], totalRows='1'),
+        ]
+
+    def api_request(method, path, query_params=None, data=None, **kw):
+        calls = sleep.mock_calls
+        if calls:
+            datetime_helpers.utcnow.return_value += datetime.timedelta(
+                seconds=calls[-1][1][0]
+            )
+        response = responses.pop(0)
+        if data:
+            response['jobReference'] = data['jobReference']
+        else:
+            response['jobReference'] = dict(jobId=path.split('/')[-1],
+                                            projectId='PROJECT')
+        return response
+
+    conn.api_request.side_effect = api_request
+    result = job.result()
+    assert result.total_rows == 1
+    assert not responses  # We made all the calls we expected to.
+    assert job.job_id != orig_job_id
+

60008525073d7260ff6b2b7230f657d6c93695d2
Tim Swast <swast@google.com>
2021-08-12 14:54:45 -0500

    format tests

--- tests/system/test_list_rows.py
+++ tests/system/test_list_rows.py
@@ -54,10 +54,8 @@ def test_list_rows_page_size(bigquery_client: bigquery.Client, table_id: str):
 
 
 def test_list_rows_scalars(bigquery_client: bigquery.Client, scalars_table: str):
-    rows = list(
-        sorted(
-            bigquery_client.list_rows(scalars_table), key=lambda row: row["rowindex"]
-        )
+    rows = sorted(
+        bigquery_client.list_rows(scalars_table), key=lambda row: row["rowindex"]
     )
     row = rows[0]
     assert row["bool_col"]  # True
@@ -86,11 +84,9 @@ def test_list_rows_scalars(bigquery_client: bigquery.Client, scalars_table: str)
 def test_list_rows_scalars_extreme(
     bigquery_client: bigquery.Client, scalars_extreme_table: str
 ):
-    rows = list(
-        sorted(
-            bigquery_client.list_rows(scalars_extreme_table),
-            key=lambda row: row["rowindex"],
-        )
+    rows = sorted(
+        bigquery_client.list_rows(scalars_extreme_table),
+        key=lambda row: row["rowindex"],
     )
     row = rows[0]
     assert row["bool_col"]  # True
@@ -99,12 +95,8 @@ def test_list_rows_scalars_extreme(
     assert row["datetime_col"] == datetime.datetime(9999, 12, 31, 23, 59, 59, 999999)
     assert row["geography_col"] == "POINT(-135 90)"
     assert row["int64_col"] == 9223372036854775807
-    assert row["numeric_col"] == decimal.Decimal(
-        "9.9999999999999999999999999999999999999E+28"
-    )
-    assert row["bignumeric_col"] == decimal.Decimal(
-        "9.999999999999999999999999999999999999999999999999999999999999999999999999999E+37"
-    )
+    assert row["numeric_col"] == decimal.Decimal(f"9.{'9' * 37}E+28")
+    assert row["bignumeric_col"] == decimal.Decimal(f"9.{'9' * 75}E+37")
     assert row["float64_col"] == float("Inf")
     assert row["string_col"] == "Hello, World"
     assert row["time_col"] == datetime.time(23, 59, 59, 999999)

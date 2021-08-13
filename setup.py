ebc57728859e69d4c053dde35dd11e9189ce081d
Bu Sun Kim <busunkim@google.com>
2021-08-13 17:31:44 +0000

    fix(deps): require pyarrow>=3.0.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ extras = {
         # grpc.Channel.close() method isn't added until 1.32.0.
         # https://github.com/grpc/grpc/pull/15254
         "grpcio >= 1.38.1, < 2.0dev",
-        "pyarrow >= 1.0.0, < 6.0dev",
+        "pyarrow >= 3.0.0, < 6.0dev",
     ],
     "pandas": ["pandas>=0.23.0", "pyarrow >= 1.0.0, < 6.0dev"],
     "bignumeric_type": ["pyarrow >= 3.0.0, < 6.0dev"],

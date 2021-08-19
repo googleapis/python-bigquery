a2011880764e8bf7abccd1d02bb50ddfd521b330
Peter Lamut <inbox@peterlamut.com>
2021-08-19 14:28:31 +0200

    Enhance standard Sql model classes

--- google/cloud/bigquery/types/standard_sql.py
+++ google/cloud/bigquery/types/standard_sql.py
@@ -14,29 +14,34 @@
 
 from dataclasses import dataclass
 import enum
-from typing import Iterable, Optional
+from typing import Any, Dict, Iterable, Optional
 
 
 @dataclass
 class StandardSqlDataType:
     """The type of a variable, e.g., a function argument.
 
+    See:
+    https://cloud.google.com/bigquery/docs/reference/rest/v2/StandardSqlDataType
+
     Examples:
 
-    INT64: {type_kind="INT64"}
-    ARRAY: {type_kind="ARRAY", array_element_type="STRING"}
-    STRUCT<x STRING, y ARRAY>: {
-        type_kind="STRUCT",
-        struct_type={
-            fields=[
-                {name="x", type={type_kind="STRING"}},
-                {
-                    name="y",
-                    type={type_kind="ARRAY", array_element_type="DATE"}
-                }
-            ]
+    .. code-block:: text
+
+        INT64: {type_kind="INT64"}
+        ARRAY: {type_kind="ARRAY", array_element_type="STRING"}
+        STRUCT<x STRING, y ARRAY>: {
+            type_kind="STRUCT",
+            struct_type={
+                fields=[
+                    {name="x", type={type_kind="STRING"}},
+                    {
+                        name="y",
+                        type={type_kind="ARRAY", array_element_type="DATE"}
+                    }
+                ]
+            }
         }
-    }
     """
 
     class TypeKind(str, enum.Enum):
@@ -78,10 +83,61 @@ class StandardSqlDataType:
                 "array_element_type and struct_type are mutally exclusive."
             )
 
+    def to_api_repr(self) -> Dict[str, Any]:
+        """Construct the API resource representation of this SQL data type."""
+
+        if not self.type_kind:
+            type_kind = self.TypeKind.TYPE_KIND_UNSPECIFIED.value
+        else:
+            type_kind = self.type_kind.value
+
+        result = {"typeKind": type_kind}
+
+        if self.type_kind == self.TypeKind.ARRAY:
+            if not self.array_element_type:
+                array_type = None
+            else:
+                array_type = self.array_element_type.to_api_repr()
+            result["arrayElementType"] = array_type
+        elif self.type_kind == self.TypeKind.STRUCT:
+            if not self.struct_type:
+                struct_type = None
+            else:
+                struct_type = self.struct_type.to_api_repr()
+            result["structType"] = struct_type
+
+        return result
+
+    @classmethod
+    def from_api_repr(cls, resource: Dict[str, Any]):
+        """Construct an SQL data type instance given its API representation."""
+        type_kind = resource.get("typeKind")
+        if type_kind not in cls.TypeKind.__members__:
+            type_kind = cls.TypeKind.TYPE_KIND_UNSPECIFIED
+        else:
+            # Convert string to an enum member.
+            type_kind = cls.TypeKind[type_kind]  # pytype: disable=missing-parameter
+
+        array_element_type = None
+        if type_kind == cls.type_kind.ARRAY:
+            element_type = resource.get("arrayElementType", {})
+            array_element_type = cls.from_api_repr(element_type)
+
+        struct_type = None
+        if type_kind == cls.TypeKind.STRUCT:
+            struct_info = resource.get("structType", {})
+            struct_type = StandardSqlStructType.from_api_repr(struct_info)
+
+        return cls(type_kind, array_element_type, struct_type)
+
 
 @dataclass
 class StandardSqlField:
-    """A field or a column."""
+    """A field or a column.
+
+    See:
+    https://cloud.google.com/bigquery/docs/reference/rest/v2/StandardSqlField
+    """
 
     name: Optional[str] = None
     """The name of this field. Can be absent for struct fields."""
@@ -93,23 +149,76 @@ class StandardSqlField:
     output parameter does not have this "type" field).
     """
 
+    def to_api_repr(self) -> Dict[str, Any]:
+        """Construct the API resource representation of this SQL field."""
+        type_repr = None if self.type is None else self.type.to_api_repr()
+        return {"name": self.name, "type": type_repr}
+
+    @classmethod
+    def from_api_repr(cls, resource: Dict[str, Any]):
+        """Construct an SQL field instance given its API representation."""
+        result = cls(
+            name=resource.get("name"),
+            type=StandardSqlDataType.from_api_repr(resource.get("type", {})),
+        )
+        return result
+
 
 @dataclass(init=False)
 class StandardSqlStructType:
-    """Type of a struct field."""
+    """Type of a struct field.
+
+    See:
+    https://cloud.google.com/bigquery/docs/reference/rest/v2/StandardSqlDataType#StandardSqlStructType
+    """
 
     fields: Optional[Iterable["StandardSqlField"]]
 
     def __init__(self, fields=None):
         self.fields = [] if fields is None else [field for field in fields]
 
+    def to_api_repr(self) -> Dict[str, Any]:
+        """Construct the API resource representation of this SQL struct type."""
+        fields = [field.to_api_repr() for field in self.fields]
+        result = {"fields": fields}
+        return result
+
+    @classmethod
+    def from_api_repr(cls, resource: Dict[str, Any]) -> "StandardSqlStructType":
+        """Construct an SQL struct type instance given its API representation."""
+        fields = (
+            StandardSqlField.from_api_repr(field)
+            for field in resource.get("fields", [])
+        )
+        return cls(fields=fields)
+
 
 @dataclass(init=False)
 class StandardSqlTableType:
-    """A table type"""
+    """A table type.
+
+    https://cloud.google.com/workflows/docs/reference/googleapis/bigquery/v2/Overview#StandardSqlTableType
+    """
 
     columns: Iterable[StandardSqlField]
     """The columns in this table type"""
 
-    def __init__(self, columns):
+    def __init__(self, columns: Iterable[StandardSqlField]):
         self.columns = [col for col in columns]
+
+    def to_api_repr(self) -> Dict[str, Any]:
+        """Construct the API resource representation of this SQL table type."""
+        columns = [col.to_api_repr() for col in self.columns]
+        return {"columns": columns}
+
+    @classmethod
+    def from_api_repr(cls, resource: Dict[str, Any]) -> "StandardSqlTableType":
+        """Construct an SQL table type instance given its API representation."""
+        columns = (
+            StandardSqlField(
+                name=column.get("name"),
+                type=StandardSqlDataType.from_api_repr(column.get("type", {})),
+            )
+            for column in resource.get("columns", [])
+        )
+        return cls(columns=columns)
---
+++ tests/unit/test_standard_sql_types.py
@@ -0,0 +1,378 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+
+class TestStandardSqlDataType:
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.bigquery.types.standard_sql import StandardSqlDataType
+
+        return StandardSqlDataType
+
+    def _make_one(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def test_ctor_w_both_array_element_and_struct_types(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+        from google.cloud.bigquery.types.standard_sql import StandardSqlStructType
+
+        StandardSqlDataType = self._get_target_class()
+        TypeKind = StandardSqlDataType.TypeKind
+
+        array_element_type = StandardSqlDataType(TypeKind.INT64)
+        struct_type = StandardSqlStructType(
+            fields=[
+                StandardSqlField("name", StandardSqlDataType(TypeKind.STRING)),
+                StandardSqlField("age", StandardSqlDataType(TypeKind.INT64)),
+            ]
+        )
+
+        with pytest.raises(ValueError, match=r".*mutally exclusive.*"):
+            self._make_one(
+                type_kind=TypeKind.TYPE_KIND_UNSPECIFIED,
+                array_element_type=array_element_type,
+                struct_type=struct_type,
+            )
+
+    def test_ctor_default_type_kind(self):
+        TypeKind = self._get_target_class().TypeKind
+        instance = self._make_one()
+        assert instance.type_kind == TypeKind.TYPE_KIND_UNSPECIFIED
+
+    def test_to_api_repr_no_type_set(self):
+        instance = self._make_one()
+        instance.type_kind = None
+
+        result = instance.to_api_repr()
+
+        assert result == {"typeKind": "TYPE_KIND_UNSPECIFIED"}
+
+    def test_to_api_repr_scalar_type(self):
+        TypeKind = self._get_target_class().TypeKind
+        instance = self._make_one(TypeKind.FLOAT64)
+
+        result = instance.to_api_repr()
+
+        assert result == {"typeKind": "FLOAT64"}
+
+    def test_to_api_repr_array_type_element_type_missing(self):
+        TypeKind = self._get_target_class().TypeKind
+        instance = self._make_one(TypeKind.ARRAY, array_element_type=None)
+
+        result = instance.to_api_repr()
+
+        expected = {"typeKind": "ARRAY", "arrayElementType": None}
+        assert result == expected
+
+    def test_to_api_repr_array_type_w_element_type(self):
+        TypeKind = self._get_target_class().TypeKind
+
+        array_element_type = self._make_one(type_kind=TypeKind.BOOL)
+        instance = self._make_one(TypeKind.ARRAY, array_element_type=array_element_type)
+
+        result = instance.to_api_repr()
+
+        expected = {"typeKind": "ARRAY", "arrayElementType": {"typeKind": "BOOL"}}
+        assert result == expected
+
+    def test_to_api_repr_struct_type_field_types_missing(self):
+        TypeKind = self._get_target_class().TypeKind
+        instance = self._make_one(TypeKind.STRUCT, struct_type=None)
+
+        result = instance.to_api_repr()
+
+        assert result == {"typeKind": "STRUCT", "structType": None}
+
+    def test_to_api_repr_struct_type_w_field_types(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+        from google.cloud.bigquery.types.standard_sql import StandardSqlStructType
+
+        StandardSqlDataType = self._get_target_class()
+        TypeKind = StandardSqlDataType.TypeKind
+
+        person_type = StandardSqlStructType(
+            fields=[
+                StandardSqlField("name", StandardSqlDataType(TypeKind.STRING)),
+                StandardSqlField("age", StandardSqlDataType(TypeKind.INT64)),
+            ]
+        )
+        employee_type = StandardSqlStructType(
+            fields=[
+                StandardSqlField("job_title", StandardSqlDataType(TypeKind.STRING)),
+                StandardSqlField("salary", StandardSqlDataType(TypeKind.FLOAT64)),
+                StandardSqlField(
+                    "employee_info",
+                    StandardSqlDataType(
+                        type_kind=TypeKind.STRUCT, struct_type=person_type,
+                    ),
+                ),
+            ]
+        )
+
+        instance = self._make_one(TypeKind.STRUCT, struct_type=employee_type)
+        result = instance.to_api_repr()
+
+        expected = {
+            "typeKind": "STRUCT",
+            "structType": {
+                "fields": [
+                    {"name": "job_title", "type": {"typeKind": "STRING"}},
+                    {"name": "salary", "type": {"typeKind": "FLOAT64"}},
+                    {
+                        "name": "employee_info",
+                        "type": {
+                            "typeKind": "STRUCT",
+                            "structType": {
+                                "fields": [
+                                    {"name": "name", "type": {"typeKind": "STRING"}},
+                                    {"name": "age", "type": {"typeKind": "INT64"}},
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        }
+        assert result == expected
+
+    def test_from_api_repr_empty_resource(self):
+        klass = self._get_target_class()
+        result = klass.from_api_repr(resource={})
+
+        expected = klass(
+            type_kind=klass.TypeKind.TYPE_KIND_UNSPECIFIED,
+            array_element_type=None,
+            struct_type=None,
+        )
+        assert result == expected
+
+    def test_from_api_repr_scalar_type(self):
+        klass = self._get_target_class()
+        resource = {"typeKind": "DATE"}
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=klass.TypeKind.DATE, array_element_type=None, struct_type=None,
+        )
+        assert result == expected
+
+    def test_from_api_repr_array_type_full(self):
+        klass = self._get_target_class()
+        resource = {"typeKind": "ARRAY", "arrayElementType": {"typeKind": "BYTES"}}
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=klass.TypeKind.ARRAY,
+            array_element_type=klass(type_kind=klass.TypeKind.BYTES),
+            struct_type=None,
+        )
+        assert result == expected
+
+    def test_from_api_repr_array_type_missing_element_type(self):
+        klass = self._get_target_class()
+        resource = {"typeKind": "ARRAY"}
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=klass.TypeKind.ARRAY,
+            array_element_type=klass(
+                type_kind=klass.TypeKind.TYPE_KIND_UNSPECIFIED,
+                array_element_type=None,
+                struct_type=None,
+            ),
+            struct_type=None,
+        )
+        assert result == expected
+
+    def test_from_api_repr_struct_type_nested(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+        from google.cloud.bigquery.types.standard_sql import StandardSqlStructType
+
+        klass = self._get_target_class()
+        TypeKind = klass.TypeKind
+
+        resource = {
+            "typeKind": "STRUCT",
+            "structType": {
+                "fields": [
+                    {"name": "job_title", "type": {"typeKind": "STRING"}},
+                    {"name": "salary", "type": {"typeKind": "FLOAT64"}},
+                    {
+                        "name": "employee_info",
+                        "type": {
+                            "typeKind": "STRUCT",
+                            "structType": {
+                                "fields": [
+                                    {"name": "name", "type": {"typeKind": "STRING"}},
+                                    {"name": "age", "type": {"typeKind": "INT64"}},
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        }
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=TypeKind.STRUCT,
+            struct_type=StandardSqlStructType(
+                fields=[
+                    StandardSqlField("job_title", klass(TypeKind.STRING)),
+                    StandardSqlField("salary", klass(TypeKind.FLOAT64)),
+                    StandardSqlField(
+                        "employee_info",
+                        klass(
+                            type_kind=TypeKind.STRUCT,
+                            struct_type=StandardSqlStructType(
+                                fields=[
+                                    StandardSqlField("name", klass(TypeKind.STRING)),
+                                    StandardSqlField("age", klass(TypeKind.INT64)),
+                                ]
+                            ),
+                        ),
+                    ),
+                ]
+            ),
+        )
+        assert result == expected
+
+    def test_from_api_repr_struct_type_missing_struct_info(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlStructType
+
+        klass = self._get_target_class()
+        resource = {"typeKind": "STRUCT"}
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=klass.TypeKind.STRUCT,
+            array_element_type=None,
+            struct_type=StandardSqlStructType(fields=[]),
+        )
+        assert result == expected
+
+    def test_from_api_repr_struct_type_incomplete_field_info(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+        from google.cloud.bigquery.types.standard_sql import StandardSqlStructType
+
+        klass = self._get_target_class()
+        TypeKind = klass.TypeKind
+
+        resource = {
+            "typeKind": "STRUCT",
+            "structType": {
+                "fields": [
+                    {"type": {"typeKind": "STRING"}},  # missing name
+                    {"name": "salary"},  # missing type
+                ],
+            },
+        }
+
+        result = klass.from_api_repr(resource=resource)
+
+        expected = klass(
+            type_kind=TypeKind.STRUCT,
+            struct_type=StandardSqlStructType(
+                fields=[
+                    StandardSqlField(None, klass(TypeKind.STRING)),
+                    StandardSqlField("salary", klass(TypeKind.TYPE_KIND_UNSPECIFIED)),
+                ]
+            ),
+        )
+        assert result == expected
+
+
+class TestStandardSqlTableType:
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.bigquery.types.standard_sql import StandardSqlTableType
+
+        return StandardSqlTableType
+
+    def _make_one(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def test_columns_shallow_copy(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+
+        columns = [
+            StandardSqlField("foo"),
+            StandardSqlField("bar"),
+            StandardSqlField("baz"),
+        ]
+
+        instance = self._make_one(columns=columns)
+
+        assert len(instance.columns) == 3
+        columns.pop()
+        assert len(instance.columns) == 3  # Still the same.
+
+    def test_to_api_repr_no_columns(self):
+        instance = self._make_one(columns=[])
+        result = instance.to_api_repr()
+        assert result == {"columns": []}
+
+    def test_to_api_repr_with_columns(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+
+        columns = [StandardSqlField("foo"), StandardSqlField("bar")]
+        instance = self._make_one(columns=columns)
+
+        result = instance.to_api_repr()
+
+        expected = {
+            "columns": [{"name": "foo", "type": None}, {"name": "bar", "type": None}]
+        }
+        assert result == expected
+
+    def test_from_api_repr_missing_columns(self):
+        resource = {}
+        result = self._get_target_class().from_api_repr(resource)
+        assert result.columns == []
+
+    def test_from_api_repr_with_incomplete_columns(self):
+        from google.cloud.bigquery.types.standard_sql import StandardSqlDataType
+        from google.cloud.bigquery.types.standard_sql import StandardSqlField
+
+        resource = {
+            "columns": [
+                {"type": {"typeKind": "BOOL"}},  # missing name
+                {"name": "bar"},  # missing type
+            ]
+        }
+
+        result = self._get_target_class().from_api_repr(resource)
+
+        assert len(result.columns) == 2
+
+        expected = StandardSqlField(
+            name=None,
+            type=StandardSqlDataType(type_kind=StandardSqlDataType.TypeKind.BOOL),
+        )
+        assert result.columns[0] == expected
+
+        expected = StandardSqlField(
+            name="bar",
+            type=StandardSqlDataType(
+                type_kind=StandardSqlDataType.TypeKind.TYPE_KIND_UNSPECIFIED
+            ),
+        )
+        assert result.columns[1] == expected

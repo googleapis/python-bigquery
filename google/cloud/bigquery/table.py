53ed313794bb4a1eba9df20ee3208f470ea12cfb
Jim Fulton <jim@jimfulton.info>
2021-09-07 08:46:43 -0600

    Add comment addressing explaining 2 checks

--- google/cloud/bigquery/table.py
+++ google/cloud/bigquery/table.py
@@ -1813,7 +1813,11 @@ class RowIterator(HTTPIterator):
         if record_batches and bqstorage_client is not None:
             return pyarrow.Table.from_batches(record_batches)
         else:
-            # No records, use schema based on BigQuery schema.
+            # No records (not record_batches), use schema based on BigQuery schema
+            # **or**
+            # we used the REST API (bqstorage_client is None),
+            # which doesn't add arrow extension metadata, so we let
+            # `bq_to_arrow_schema` do it.
             arrow_schema = _pandas_helpers.bq_to_arrow_schema(self._schema)
             return pyarrow.Table.from_batches(record_batches, schema=arrow_schema)
 

455cba441624d699d4ffd05dbfd57f87faf50314
Tim Swast <swast@google.com>
2021-10-27 17:02:08 -0500

    chore: cleanup intersphinx links

--- owlbot.py
+++ owlbot.py
@@ -32,8 +32,6 @@ templated_files = common.py_library(
     intersphinx_dependencies={
         "pandas": "http://pandas.pydata.org/pandas-docs/stable/",
         "geopandas": "https://geopandas.org/",
-        "proto-plus": ("https://proto-plus-python.readthedocs.io/en/latest/", None),
-        "protobuf": ("https://googleapis.dev/python/protobuf/latest/", None),
     },
 )
 
@@ -54,9 +52,6 @@ s.move(
         "renovate.json",
     ],
 )
-
-# Remove unneeded intersphinx links, the library does not use any proto-generated code.
-s.replace("docs/conf.py", r'\s+"(proto-plus|protobuf)":.*$', "")
 
 
 # ----------------------------------------------------------------------------

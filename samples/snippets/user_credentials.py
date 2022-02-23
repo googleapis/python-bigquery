533bcacb657d2ac29df3cc23cde4b852a413667e
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-02-23 16:59:35 -0700

    Update samples/snippets/user_credentials.py
    Co-authored-by: Tim Swast <swast@google.com>

--- samples/snippets/user_credentials.py
+++ samples/snippets/user_credentials.py
@@ -32,6 +32,12 @@ def main(project):
         "client_secrets.json", scopes=["https://www.googleapis.com/auth/bigquery"]
     )
 
+    # This launches a local server to be used as the callback URL in the desktop
+    # app auth flow. If you are accessing the application remotely, such as over
+    # SSH or a remote Jupyter notebook, this flow will not work. Use the
+    # `gcloud auth application-default login --no-browser` command or workload
+    # identity federation to get authentication tokens, instead.
+    # 
     appflow.run_local_server()
 
     credentials = appflow.credentials

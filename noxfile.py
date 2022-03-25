34d9beca7d9aab5a4871c819a5cd2434f48a090d
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-03-25 15:19:39 -0600

    fix(deps): require google-api-core >= 1.31.5, >= 2.3.2 on v1 release (#1166)
    * chore(deps): allow google-api-core v2 on v1 release
    
    * chore: also update cloud-core
    
    * test: update tests to support latest google-cloud-core (#276)
    
    `google-cloud-core` version 1.4.2 populates `prettyPrint=false` by
    default. Update the connection tests to expect a value for
    `prettyPrint`.
    
    * ci: fix docs build
    
    * chore: update system tests and samples to use and @google.com email address (#942)
    
    * chore: update system tests and samples to use and @google.com email address
    
    * Add group prefix
    
    * fixed access entry some more
    
    Co-authored-by: Tim Swast <swast@google.com>
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>
    Co-authored-by: Jim Fulton <jim@jimfulton.info>

--- noxfile.py
+++ noxfile.py
@@ -206,7 +206,7 @@ def blacken(session):
 def docs(session):
     """Build the docs."""
 
-    session.install("ipython", "recommonmark", "sphinx", "sphinx_rtd_theme")
+    session.install("ipython", "recommonmark", "sphinx<3.0.0", "sphinx_rtd_theme")
     session.install("google-cloud-storage")
     session.install("-e", ".[all]")
 
@@ -216,39 +216,6 @@ def docs(session):
         "-W",  # warnings as errors
         "-T",  # show full traceback on exception
         "-N",  # no colors
-        "-b",
-        "html",
-        "-d",
-        os.path.join("docs", "_build", "doctrees", ""),
-        os.path.join("docs", ""),
-        os.path.join("docs", "_build", "html", ""),
-    )
-
-
-@nox.session(python="3.8")
-def docfx(session):
-    """Build the docfx yaml files for this library."""
-
-    session.install("-e", ".")
-    session.install("sphinx", "alabaster", "recommonmark", "sphinx-docfx-yaml")
-
-    shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
-    session.run(
-        "sphinx-build",
-        "-T",  # show full traceback on exception
-        "-N",  # no colors
-        "-D",
-        (
-            "extensions=sphinx.ext.autodoc,"
-            "sphinx.ext.autosummary,"
-            "docfx_yaml.extension,"
-            "sphinx.ext.intersphinx,"
-            "sphinx.ext.coverage,"
-            "sphinx.ext.napoleon,"
-            "sphinx.ext.todo,"
-            "sphinx.ext.viewcode,"
-            "recommonmark"
-        ),
         "-b",
         "html",
         "-d",

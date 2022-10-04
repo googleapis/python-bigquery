5d978176b7818bc2043c5ea48a02a64f9de2202c
Chalmer <chalmerlowe@google.com>
2022-10-04 20:05:02 +0000

    fix: refactored to account for changes in dependency

--- google/cloud/bigquery/table.py
+++ google/cloud/bigquery/table.py
@@ -43,7 +43,9 @@ try:
 except ImportError:
     shapely = None
 else:
-    _read_wkt = shapely.geos.WKTReader(shapely.geos.lgeos).read
+    #_read_wkt = shapely.geos.WKTReader(shapely.geos.lgeos).read
+    _read_wkt = shapely.io.from_wkt
+
 
 import google.api_core.exceptions
 from google.api_core.page_iterator import HTTPIterator

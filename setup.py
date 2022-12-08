b85dcf351b9df0375c9a3c85e8d683e1865a9737
Tim Swast <swast@google.com>
2022-12-08 12:26:49 -0600

    adjust shapely

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ extras = {
         "db-dtypes>=0.3.0,<2.0.0dev",
     ],
     "ipywidgets": ["ipywidgets==7.7.1"],
-    "geopandas": ["geopandas>=0.9.0, <1.0dev", "Shapely>=1.6.4.post2, <2.0dev"],
+    "geopandas": ["geopandas>=0.9.0, <1.0dev", "Shapely>=1.8.4, <2.0dev"],
     "ipython": ["ipython>=7.0.1,!=8.1.0"],
     "tqdm": ["tqdm >= 4.7.4, <5.0.0dev"],
     "opentelemetry": [

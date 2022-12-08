d71141d881f33f4bc6dfe99b362e061ce14e1ad7
Tim Swast <swast@google.com>
2022-12-08 11:28:20 -0600

    restore optional bqstorage_client

--- google/cloud/bigquery/job/query.py
+++ google/cloud/bigquery/job/query.py
@@ -1537,7 +1537,7 @@ class QueryJob(_AsyncJob):
     def to_arrow(
         self,
         progress_bar_type: str = None,
-        bqstorage_client: "bigquery_storage.BigQueryReadClient" = None,
+        bqstorage_client: Optional["bigquery_storage.BigQueryReadClient"] = None,
         create_bqstorage_client: bool = True,
         max_results: Optional[int] = None,
     ) -> "pyarrow.Table":
@@ -1611,7 +1611,7 @@ class QueryJob(_AsyncJob):
     # that should only exist here in the QueryJob method.
     def to_dataframe(
         self,
-        bqstorage_client: "bigquery_storage.BigQueryReadClient" = None,
+        bqstorage_client: Optional["bigquery_storage.BigQueryReadClient"] = None,
         dtypes: Dict[str, Any] = None,
         progress_bar_type: str = None,
         create_bqstorage_client: bool = True,

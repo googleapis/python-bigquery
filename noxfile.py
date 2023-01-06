d254f4de9ce0e559df7969b330e4164e7d1000e7
Jared Chapman <thejaredchapman@gmail.com>
2023-01-06 11:36:30 -0600

    chore: install all sample dependencies when type checking samples

--- noxfile.py
+++ noxfile.py
@@ -200,19 +200,29 @@ def mypy_samples(session):
     """Run type checks with mypy."""
     session.install("-e", ".[all]")
 
-    session.install("ipython", "pytest")
+    session.install("pytest")
+    for requirements_path in CURRENT_DIRECTORY.glob("samples/*/requirements.txt"):
+        session.install("-r", requirements_path)
     session.install(MYPY_VERSION)
 
     # Just install the dependencies' type info directly, since "mypy --install-types"
     # might require an additional pass.
-    session.install("types-mock", "types-pytz")
+    session.install(
+        "types-mock", 
+        "types-pytz",
+        "types-protobuf",
+        "types-python-dateutil",
+        "types-requests",
+        "types-setuptools",
+    )
+
     session.install("typing-extensions")  # for TypedDict in pre-3.8 Python versions
 
     session.run(
         "mypy",
         "--config-file",
         str(CURRENT_DIRECTORY / "samples" / "mypy.ini"),
-        "--no-incremental",  # Required by warn-unused-configs from mypy.ini to work
+        "--no-incremental", # Required by warn-unused-configs from mypy.ini to work
         "samples/",
     )
 

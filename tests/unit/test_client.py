8b428b5d3f3a255c6f1f6563b4408044041c08c8
Tim Swast <swast@google.com>
2023-06-01 10:23:10 -0500

    revert most changes to the test and explain why we're looking for a different exception from the original 'conflict'

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -5087,16 +5087,18 @@ class TestClient(unittest.TestCase):
         http = object()
         client = self._make_one(project=self.PROJECT, credentials=creds, _http=http)
 
-        job_create_error = DataLoss("we lost your job, sorry")
+        job_create_error = Conflict("Job already exists.")
         job_begin_patcher = mock.patch.object(
             QueryJob, "_begin", side_effect=job_create_error
         )
         get_job_patcher = mock.patch.object(
-            client, "get_job", side_effect=Conflict("Job already exists.")
+            client, "get_job", side_effect=DataLoss("we lost your job, sorry")
         )
 
         with job_begin_patcher, get_job_patcher:
-            # If get job request fails, the original exception should be raised.
+            # If get job request fails but supposedly there does exist a job
+            # with this ID already, raise the exception explaining why we
+            # couldn't recover the job.
             with pytest.raises(DataLoss, match="we lost your job, sorry"):
                 client.query("SELECT 1;", job_id=None)
 

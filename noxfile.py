037417aa57bf5a386bad8436c5fa1715dcb06204
Chalmer <chalmerlowe@google.com>
2023-10-25 13:29:10 +0000

    tweaks to noxfile for testing purposes

--- noxfile.py
+++ noxfile.py
@@ -277,15 +277,11 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
-    import sys
-
     session.run(
         "coverage",
         "report",
         "--show-missing",
-        "--fail-under=100",
-        "--branch",
-        "--data-file=sys.stdout",
+        "--fail-under=100"
     )
     session.run("coverage", "erase")
 

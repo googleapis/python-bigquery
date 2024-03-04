8bb9046b583ec3a1ec4036e07aecc189ccb676c6
kiraksi <kirnendra@google.com>
2024-03-04 10:25:25 -0800

    feat: AsyncClient for async query_and_wait for BQ jobs

---
+++ google/cloud/bigquery/async_client.py
@@ -0,0 +1,225 @@
+from google.cloud.bigquery.client import *
+from google.cloud.bigquery import _job_helpers
+from google.cloud.bigquery import table
+import asyncio
+from google.api_core import gapic_v1, retry_async
+
+class AsyncClient(Client):  
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    async def query_and_wait(
+        self,
+        query,
+        *,
+        job_config: Optional[QueryJobConfig] = None,
+        location: Optional[str] = None,
+        project: Optional[str] = None,
+        api_timeout: TimeoutType = DEFAULT_TIMEOUT,
+        wait_timeout: TimeoutType = None,
+        retry: retries.Retry = DEFAULT_RETRY,
+        job_retry: retries.Retry = DEFAULT_JOB_RETRY,
+        page_size: Optional[int] = None,
+        max_results: Optional[int] = None,
+        ) -> RowIterator:
+
+        if project is None:
+            project = self.project
+
+        if location is None:
+            location = self.location
+
+        # if job_config is not None:
+        #     self._verify_job_config_type(job_config, QueryJobConfig)
+
+        # if job_config is not None:
+        #     self._verify_job_config_type(job_config, QueryJobConfig)
+
+        job_config = _job_helpers.job_config_with_defaults(
+            job_config, self._default_query_job_config
+        )
+
+        return await async_query_and_wait(
+            self,
+            query,
+            job_config=job_config,
+            location=location,
+            project=project,
+            api_timeout=api_timeout,
+            wait_timeout=wait_timeout,
+            retry=retry,
+            job_retry=job_retry,
+            page_size=page_size,
+            max_results=max_results,
+        )
+
+
+async def async_query_and_wait(    
+    client: "Client",
+    query: str,
+    *,
+    job_config: Optional[job.QueryJobConfig],
+    location: Optional[str],
+    project: str,
+    api_timeout: Optional[float] = None,
+    wait_timeout: Optional[float] = None,
+    retry: Optional[retries.Retry],
+    job_retry: Optional[retries.Retry],
+    page_size: Optional[int] = None,
+    max_results: Optional[int] = None,
+) -> table.RowIterator:
+            
+    # Some API parameters aren't supported by the jobs.query API. In these
+    # cases, fallback to a jobs.insert call.
+    if not _job_helpers._supported_by_jobs_query(job_config):
+        return await async_wait_or_cancel(
+            _job_helpers.query_jobs_insert(
+                client=client,
+                query=query,
+                job_id=None,
+                job_id_prefix=None,
+                job_config=job_config,
+                location=location,
+                project=project,
+                retry=retry,
+                timeout=api_timeout,
+                job_retry=job_retry,
+            ),
+            api_timeout=api_timeout,
+            wait_timeout=wait_timeout,
+            retry=retry,
+            page_size=page_size,
+            max_results=max_results,
+        )
+
+    path = _job_helpers._to_query_path(project)
+    request_body = _job_helpers._to_query_request(
+        query=query, job_config=job_config, location=location, timeout=api_timeout
+    )
+
+    if page_size is not None and max_results is not None:
+        request_body["maxResults"] = min(page_size, max_results)
+    elif page_size is not None or max_results is not None:
+        request_body["maxResults"] = page_size or max_results
+
+    if os.getenv("QUERY_PREVIEW_ENABLED", "").casefold() == "true":
+        request_body["jobCreationMode"] = "JOB_CREATION_OPTIONAL"
+
+    async def do_query():
+        request_body["requestId"] = _job_helpers.make_job_id()
+        span_attributes = {"path": path}
+
+        # Wrap the RPC method; this adds retry and timeout information,
+        # and friendly error handling.
+        rpc = gapic_v1.method_async.wrap_method(
+            client._call_api,
+            default_retry=retry_async.AsyncRetry(
+                initial=0.1,
+                maximum=60.0,
+                multiplier=1.3,
+                predicate=retries.if_exception_type(
+                    core_exceptions.ServiceUnavailable,
+                ),
+                deadline=60.0,
+            ),
+            default_timeout=60.0,
+            client_info=DEFAULT_CLIENT_INFO,
+        )
+
+        # For easier testing, handle the retries ourselves.
+        # if retry is not None:
+        #     response = retry(client._call_api)(
+        #         retry=None,  # We're calling the retry decorator ourselves.
+        #         span_name="BigQuery.query",
+        #         span_attributes=span_attributes,
+        #         method="POST",
+        #         path=path,
+        #         data=request_body,
+        #         timeout=api_timeout,
+        #     )
+        # else:
+        response = await rpc(
+            retry=None,
+            span_name="BigQuery.query",
+            span_attributes=span_attributes,
+            method="POST",
+            path=path,
+            data=request_body,
+            timeout=api_timeout,
+        )
+
+        # Even if we run with JOB_CREATION_OPTIONAL, if there are more pages
+        # to fetch, there will be a job ID for jobs.getQueryResults.
+        query_results = google.cloud.bigquery.query._QueryResults.from_api_repr(
+            response
+        )
+        page_token = query_results.page_token
+        more_pages = page_token is not None
+
+        if more_pages or not query_results.complete:
+            # TODO(swast): Avoid a call to jobs.get in some cases (few
+            # remaining pages) by waiting for the query to finish and calling
+            # client._list_rows_from_query_results directly. Need to update
+            # RowIterator to fetch destination table via the job ID if needed.
+            return await async_wait_or_cancel(
+                _job_helpers._to_query_job(client, query, job_config, response),
+                api_timeout=api_timeout,
+                wait_timeout=wait_timeout,
+                retry=retry,
+                page_size=page_size,
+                max_results=max_results,
+            )
+
+        return table.RowIterator(
+            client=client,
+            api_request=functools.partial(client._call_api, retry, timeout=api_timeout),
+            path=None,
+            schema=query_results.schema,
+            max_results=max_results,
+            page_size=page_size,
+            total_rows=query_results.total_rows,
+            first_page_response=response,
+            location=query_results.location,
+            job_id=query_results.job_id,
+            query_id=query_results.query_id,
+            project=query_results.project,
+            num_dml_affected_rows=query_results.num_dml_affected_rows,
+        )
+    
+
+    if job_retry is not None:
+        return job_retry(do_query)()
+    else:
+        return do_query()
+
+async def async_wait_or_cancel(
+    job: job.QueryJob,
+    api_timeout: Optional[float],
+    wait_timeout: Optional[float],
+    retry: Optional[retries.Retry],
+    page_size: Optional[int],
+    max_results: Optional[int],
+) -> table.RowIterator:
+    try:
+        return await job.result(
+            page_size=page_size,
+            max_results=max_results,
+            retry=retry,
+            timeout=wait_timeout,
+        )
+    except Exception:
+        # Attempt to cancel the job since we can't return the results.
+        try:
+            job.cancel(retry=retry, timeout=api_timeout)
+        except Exception:
+            # Don't eat the original exception if cancel fails.
+            pass
+        raise
+
+
+DEFAULT_CLIENT_INFO = gapic_v1.client_info.ClientInfo(
+    "3.17.2"
+)
+
+__all__ = ("AsyncClient",)
---
+++ tests/unit/test_async_client.py
@@ -0,0 +1,324 @@
+import copy
+import collections
+import datetime
+import decimal
+import email
+import gzip
+import http.client
+import io
+import itertools
+import json
+import operator
+import unittest
+import warnings
+
+import mock
+import requests
+import packaging
+import pytest
+import sys
+import inspect
+
+if sys.version_info >= (3, 9):
+    import asyncio
+
+try:
+    import importlib.metadata as metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+try:
+    import pandas
+except (ImportError, AttributeError):  # pragma: NO COVER
+    pandas = None
+
+try:
+    import opentelemetry
+except ImportError:
+    opentelemetry = None
+
+if opentelemetry is not None:
+    try:
+        from opentelemetry import trace
+        from opentelemetry.sdk.trace import TracerProvider
+        from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+        from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
+            InMemorySpanExporter,
+        )
+    except (ImportError, AttributeError) as exc:  # pragma: NO COVER
+        msg = "Error importing from opentelemetry, is the installed version compatible?"
+        raise ImportError(msg) from exc
+
+try:
+    import pyarrow
+except (ImportError, AttributeError):  # pragma: NO COVER
+    pyarrow = None
+
+import google.api_core.exceptions
+from google.api_core import client_info
+import google.cloud._helpers
+from google.cloud import bigquery
+
+from google.cloud.bigquery.dataset import DatasetReference
+from google.cloud.bigquery import exceptions
+from google.cloud.bigquery import ParquetOptions
+from google.cloud.bigquery.retry import DEFAULT_TIMEOUT
+import google.cloud.bigquery.table
+
+try:
+    from google.cloud import bigquery_storage
+except (ImportError, AttributeError):  # pragma: NO COVER
+    bigquery_storage = None
+from test_utils.imports import maybe_fail_import
+from tests.unit.helpers import make_connection
+
+if pandas is not None:
+    PANDAS_INSTALLED_VERSION = metadata.version("pandas")
+else:
+    PANDAS_INSTALLED_VERSION = "0.0.0"
+
+def asyncio_run(async_func):
+    def wrapper(*args, **kwargs):
+        return asyncio.run(async_func(*args, **kwargs))
+
+    wrapper.__signature__ = inspect.signature(
+        async_func
+    )  # without this, fixtures are not injected
+
+    return wrapper
+
+
+def _make_credentials():
+    import google.auth.credentials
+
+    return mock.Mock(spec=google.auth.credentials.Credentials)
+
+
+
+class TestClient(unittest.TestCase):
+    PROJECT = "PROJECT"
+    DS_ID = "DATASET_ID"
+    TABLE_ID = "TABLE_ID"
+    MODEL_ID = "MODEL_ID"
+    TABLE_REF = DatasetReference(PROJECT, DS_ID).table(TABLE_ID)
+    KMS_KEY_NAME = "projects/1/locations/us/keyRings/1/cryptoKeys/1"
+    LOCATION = "us-central"
+
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.bigquery.async_client import AsyncClient
+
+        return AsyncClient
+
+    def _make_one(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def _make_table_resource(self):
+        return {
+            "id": "%s:%s:%s" % (self.PROJECT, self.DS_ID, self.TABLE_ID),
+            "tableReference": {
+                "projectId": self.PROJECT,
+                "datasetId": self.DS_ID,
+                "tableId": self.TABLE_ID,
+            },
+        }
+
+    def test_ctor_defaults(self):
+        from google.cloud.bigquery._http import Connection
+
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(project=self.PROJECT, credentials=creds, _http=http)
+        self.assertIsInstance(client._connection, Connection)
+        self.assertIs(client._connection.credentials, creds)
+        self.assertIs(client._connection.http, http)
+        self.assertIsNone(client.location)
+        self.assertEqual(
+            client._connection.API_BASE_URL, Connection.DEFAULT_API_ENDPOINT
+        )
+
+    def test_ctor_w_empty_client_options(self):
+        from google.api_core.client_options import ClientOptions
+
+        creds = _make_credentials()
+        http = object()
+        client_options = ClientOptions()
+        client = self._make_one(
+            project=self.PROJECT,
+            credentials=creds,
+            _http=http,
+            client_options=client_options,
+        )
+        self.assertEqual(
+            client._connection.API_BASE_URL, client._connection.DEFAULT_API_ENDPOINT
+        )
+
+    @pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires python3.9 or higher"
+    )
+    @asyncio_run
+    async def test_query_and_wait_defaults(self):
+        query = "select count(*) from `bigquery-public-data.usa_names.usa_1910_2013`"
+        jobs_query_response = {
+            "jobComplete": True,
+            "schema": {
+                "fields": [
+                    {
+                        "name": "f0_",
+                        "type": "INTEGER",
+                        "mode": "NULLABLE",
+                    },
+                ],
+            },
+            "totalRows": "1",
+            "rows": [{"f": [{"v": "5552452"}]}],
+            "queryId": "job_abcDEF_",
+        }
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(project=self.PROJECT, credentials=creds, _http=http)
+        conn = client._connection = make_connection(jobs_query_response)
+
+        rows = await client.query_and_wait(query)
+
+        self.assertIsInstance(rows, google.cloud.bigquery.table.RowIterator)
+        self.assertEqual(rows.query_id, "job_abcDEF_")
+        self.assertEqual(rows.total_rows, 1)
+        # No job reference in the response should be OK for completed query.
+        self.assertIsNone(rows.job_id)
+        self.assertIsNone(rows.project)
+        self.assertIsNone(rows.location)
+
+        # Verify the request we send is to jobs.query.
+        conn.api_request = await conn.api_request
+        conn.api_request.assert_called_once()
+        _, req = conn.api_request.call_args
+        self.assertEqual(req["method"], "POST")
+        self.assertEqual(req["path"], "/projects/PROJECT/queries")
+        self.assertEqual(req["timeout"], DEFAULT_TIMEOUT)
+        sent = req["data"]
+        self.assertEqual(sent["query"], query)
+        self.assertFalse(sent["useLegacySql"])
+
+    @pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires python3.9 or higher"
+    )
+    @asyncio_run
+    async def test_query_and_wait_w_default_query_job_config(self):
+        from google.cloud.bigquery import job
+
+        query = "select count(*) from `bigquery-public-data.usa_names.usa_1910_2013`"
+        jobs_query_response = {
+            "jobComplete": True,
+        }
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(
+            project=self.PROJECT,
+            credentials=creds,
+            _http=http,
+            default_query_job_config=job.QueryJobConfig(
+                labels={
+                    "default-label": "default-value",
+                },
+            ),
+        )
+        conn = client._connection = make_connection(jobs_query_response)
+
+        future_result = client.query_and_wait(query)
+        _ = await future_result
+
+        # Verify the request we send is to jobs.query.
+        # Instantiate my query path, dumping call stacks to see where I am. Get the address of my mocked call and actual call thats invoked, see if thats the same. See if my mocked thing is the thing getting invoked or not.
+        # conn.api_request.assert_called_once()
+        _, req = conn.api_request.call_args
+        self.assertEqual(req["method"], "POST")
+        self.assertEqual(req["path"], f"/projects/{self.PROJECT}/queries")
+        sent = req["data"]
+        self.assertEqual(sent["labels"], {"default-label": "default-value"})
+
+    @pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires python3.9 or higher"
+    )
+    @asyncio_run
+    async def test_query_and_wait_w_job_config(self):
+        from google.cloud.bigquery import job
+
+        query = "select count(*) from `bigquery-public-data.usa_names.usa_1910_2013`"
+        jobs_query_response = {
+            "jobComplete": True,
+        }
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(
+            project=self.PROJECT,
+            credentials=creds,
+            _http=http,
+        )
+        conn = client._connection = make_connection(jobs_query_response)
+
+        future_result = client.query_and_wait(
+            query,
+            job_config=job.QueryJobConfig(
+                labels={
+                    "job_config-label": "job_config-value",
+                },
+            ),
+        )
+        rows = await future_result
+
+        # Verify the request we send is to jobs.query.
+        # conn.api_request.assert_called_once()
+        _, req = conn.api_request.call_args
+        self.assertEqual(req["method"], "POST")
+        self.assertEqual(req["path"], f"/projects/{self.PROJECT}/queries")
+        sent = req["data"]
+        self.assertEqual(sent["labels"], {"job_config-label": "job_config-value"})
+
+    @pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires python3.9 or higher"
+    )
+    @asyncio_run
+    async def test_query_and_wait_w_location(self):
+        query = "select count(*) from `bigquery-public-data.usa_names.usa_1910_2013`"
+        jobs_query_response = {
+            "jobComplete": True,
+        }
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(project=self.PROJECT, credentials=creds, _http=http)
+        conn = client._connection = make_connection(jobs_query_response)
+
+        future_result = client.query_and_wait(query, location="not-the-client-location")
+        _ = await future_result
+
+        # Verify the request we send is to jobs.query.
+        # conn.api_request.assert_called_once()
+        _, req = conn.api_request.call_args
+        self.assertEqual(req["method"], "POST")
+        self.assertEqual(req["path"], f"/projects/{self.PROJECT}/queries")
+        sent = req["data"]
+        self.assertEqual(sent["location"], "not-the-client-location")
+
+    @pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires python3.9 or higher"
+    )
+    @asyncio_run
+    async def test_query_and_wait_w_project(self):
+        query = "select count(*) from `bigquery-public-data.usa_names.usa_1910_2013`"
+        jobs_query_response = {
+            "jobComplete": True,
+        }
+        creds = _make_credentials()
+        http = object()
+        client = self._make_one(project=self.PROJECT, credentials=creds, _http=http)
+        conn = client._connection = make_connection(jobs_query_response)
+
+        future_result = client.query_and_wait(query, project="not-the-client-project")
+        _ = await future_result
+
+        # Verify the request we send is to jobs.query.
+        # conn.api_request.assert_called_once()
+        _, req = conn.api_request.call_args
+        self.assertEqual(req["method"], "POST")
+        self.assertEqual(req["path"], "/projects/not-the-client-project/queries")

867e5bc89d24daf48f016100f7467611477d95c4
chalmer lowe <chalmerlowe@google.com>
2024-04-10 16:08:22 +0000

    updates Compression class

--- google/cloud/bigquery/enums.py
+++ google/cloud/bigquery/enums.py
@@ -22,7 +22,7 @@ class AutoRowIDs(enum.Enum):
     GENERATE_UUID = enum.auto()
 
 
-class Compression(enum.Enum):
+class Compression(str, enum.Enum):
     """The compression type to use for exported files. The default value is
     :attr:`NONE`.
 

4a04d47bd55cb4739f0d7b7f62dbb3094bb63e40
Seth Hollyman <shollyman@google.com>
2024-05-15 16:22:19 +0000

    docs: tweak string so we have a testable change

--- google/cloud/bigquery/client.py
+++ google/cloud/bigquery/client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Client for interacting with the Google BigQuery API."""
+"""Client for interacting with the Google BigQuery V2 API."""
 
 from __future__ import absolute_import
 from __future__ import division

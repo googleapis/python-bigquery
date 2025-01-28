afcd892cc56ae4316a973ec19229b76971b752e7
chalmer lowe <chalmerlowe@google.com>
2025-01-28 14:36:41 +0000

    Modifies docstring

--- google/cloud/bigquery/enums.py
+++ google/cloud/bigquery/enums.py
@@ -369,16 +369,16 @@ class RoundingMode(enum.Enum):
     ROUND_HALF_AWAY_FROM_ZERO: rounds half values away from zero when applying
     precision and scale upon writing of NUMERIC and BIGNUMERIC values.
     For Scale: 0
-        1.1, 1.2, 1.3, 1.4 => 1
-        1.5, 1.6, 1.7, 1.8, 1.9 => 2
+    * 1.1, 1.2, 1.3, 1.4 => 1
+    * 1.5, 1.6, 1.7, 1.8, 1.9 => 2
 
     ROUND_HALF_EVEN: rounds half values to the nearest even value when applying
     precision and scale upon writing of NUMERIC and BIGNUMERIC values.
     For Scale: 0
-        1.1, 1.2, 1.3, 1.4 => 1
-        1.5 => 2
-        1.6, 1.7, 1.8, 1.9 => 2
-        2.5 => 2
+    * 1.1, 1.2, 1.3, 1.4 => 1
+    * 1.5 => 2
+    * 1.6, 1.7, 1.8, 1.9 => 2
+    * 2.5 => 2
     """
 
     ROUNDING_MODE_UNSPECIFIED = 0

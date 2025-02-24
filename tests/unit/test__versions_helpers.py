48caeb72de1fae2d0d6dbdeaae0f18aaa5c33d52
chalmer lowe <chalmerlowe@google.com>
2025-02-24 19:29:47 +0000

    Revert test__versions_helpers.py changes related to pandas <1.5

--- tests/unit/test__versions_helpers.py
+++ tests/unit/test__versions_helpers.py
@@ -188,15 +188,17 @@ def test_bqstorage_is_read_session_optional_false():
 
 
 @pytest.mark.skipif(pandas is None, reason="pandas is not installed")
-@pytest.mark.parametrize("version", ["1.5.0", "1.7.0", "2.0.0"])
+@pytest.mark.parametrize("version", ["1.1.0", "2.0.0", "2.1.0"])
 def test_try_import_raises_no_error_w_recent_pandas(version):
+    # Comparing against the minimum allowed pandas version.
+    # As long as the installed version is greater than that, no
+    # error is raised.
     versions = _versions_helpers.PandasVersions()
     with mock.patch("pandas.__version__", new=version):
         try:
             pandas = versions.try_import(raise_if_error=True)
             assert pandas is not None
-        # This line should generally not execute, hence the pragma.
-        except exceptions.LegacyPandasError:  # pragma: NO COVER
+        except exceptions.LegacyPandasError:
             raise ("Legacy error raised with a non-legacy dependency version.")
 
 

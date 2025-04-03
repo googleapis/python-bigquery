ff2129e07c1340d739d605039c21ad5a3f0bd6f0
chalmer lowe <chalmerlowe@google.com>
2025-04-03 11:52:54 +0000

    adds pytest-xdist to nox system session for experimentation

--- noxfile.py
+++ noxfile.py
@@ -226,7 +226,12 @@ def system(session):
 
     # Install all test dependencies, then install local packages in place.
     session.install(
-        "pytest", "psutil", "google-cloud-testutils", "-c", constraints_path
+        "pytest",
+        "psutil",
+        "pytest-xdist",
+        "google-cloud-testutils",
+        "-c",
+        constraints_path,
     )
     if os.environ.get("GOOGLE_API_USE_CLIENT_CERTIFICATE", "") == "true":
         # mTLS test requires pyopenssl and latest google-cloud-storage
@@ -259,6 +264,7 @@ def system(session):
     # Run py.test against the system tests.
     session.run(
         "py.test",
+        "-n=auto",
         "--quiet",
         "-W default::PendingDeprecationWarning",
         os.path.join("tests", "system"),

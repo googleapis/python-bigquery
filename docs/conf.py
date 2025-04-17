e7b106148fd211577e5752a70602b7eebb305593
Anthonios Partheniou <partheniou@google.com>
2025-04-16 20:16:39 -0400

    remove replacement in owlbot.py

--- docs/conf.py
+++ docs/conf.py
@@ -110,12 +110,6 @@ language = None
 # directories to ignore when looking for source files.
 exclude_patterns = [
     "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
-    "google/cloud/bigquery_v2/**",  # Legacy proto-based types.
     "_build",
     "**/.nox/**/*",
     "samples/AUTHORING_GUIDE.md",

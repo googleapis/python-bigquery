b6261dc58ff954ffd281b6b34a48dce1e7a74d53
Chalmer Lowe <chalmerlowe@google.com>
2025-07-02 13:43:48 -0400

    Update google/cloud/bigquery/job/load.py

--- google/cloud/bigquery/job/load.py
+++ google/cloud/bigquery/job/load.py
@@ -558,10 +558,11 @@ class LoadJobConfig(_JobConfig):
         position. This is done to keep the behavior backward-compatible.
 
         Acceptable values are:
-            SOURCE_COLUMN_MATCH_UNSPECIFIED - Unspecified column name match option.
-            POSITION - matches by position. This assumes that the columns are ordered
+
+            SOURCE_COLUMN_MATCH_UNSPECIFIED: Unspecified column name match option.
+            POSITION: matches by position. This assumes that the columns are ordered
                 the same way as the schema.
-            NAME - matches by name. This reads the header row as column names and
+            NAME: matches by name. This reads the header row as column names and
                 reorders columns to match the field names in the schema.
         See:
         https://cloud.google.com/bigquery/docs/reference/rest/v2/Job#JobConfigurationLoad.FIELDS.source_column_match
